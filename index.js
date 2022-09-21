4cf9cfc9996e97b4fd404442ba55694286bbbe20
saem <78132205+saem2002@users.noreply.github.com>
2022-09-21 14:10:33 +0530

    st3-project

---
+++ index.js
@@ -0,0 +1,53 @@
+function calmercury(){
+    let weight = document.getElementById("hellohowareyou").value;
+    let mercuryw = weight * 0.38;
+    document.getElementById('root').innerHTML = ("Your weight on Mercury is : " + mercuryw);
+}
+
+function calvenus(){
+    let weight = document.getElementById("hellohowareyou").value;
+    let venusw = weight * 0.91;
+    document.getElementById('root').innerHTML = ("Your weight on Venus is : " + venusw);
+}
+
+function calearth(){
+    let weight = document.getElementById("hellohowareyou").value;
+    let earthw = weight * 1;
+    document.getElementById('root').innerHTML = ("Your weight on Earth is : " + earthw);
+}
+
+function calmars(){
+    let weight = document.getElementById("hellohowareyou").value;
+    let marsw = weight * 0.38;
+    document.getElementById('root').innerHTML = ("Your weight on Mars is : " + marsw);
+}
+
+function caljupiter(){
+    let weight = document.getElementById("hellohowareyou").value;
+    let jupiterw = weight * 2.34;
+    document.getElementById('root').innerHTML = ("Your weight on Jupiter is : " + jupiterw);
+}
+
+function calsaturn(){
+    let weight = document.getElementById("hellohowareyou").value;
+    let saturnw = weight * 1.06;
+    document.getElementById('root').innerHTML = ("Your weight on Saturn is : " + saturnw);
+}
+
+function caluranus() {
+    let weight = document.getElementById("hellohowareyou").value;
+    let uranusw = weight * 0.92;
+    document.getElementById('root').innerHTML = ("Your weight on Uranus is : " + uranusw);
+}
+
+function calneptune(){
+    let weight = document.getElementById("hellohowareyou").value;
+    let neptunew = weight * 1.19;
+    document.getElementById('root').innerHTML = ("Your weight on Neptune is : " + neptunew);
+}
+
+function calpluto(){
+    let weight = document.getElementById("hellohowareyou").value;
+    let plutow = weight * 0.06;
+    document.getElementById('root').innerHTML = ("Your weight on Pluto is : " + plutow);
+}
